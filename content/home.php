a54cb775f0619676db41477afcb0d596f824f477
nkaaf <nkaaf@protonmail.com>
2022-01-25 11:13:43 +0100

    fix FSAA Guide path

--- content/home.php
+++ content/home.php
@@ -26,7 +26,7 @@
 		  <li><a target="_blank" href="https://www.formula-ata.it/how-to-register">SAE Italy</a></li>
 		  <li><a target="_blank" href="https://formulastudent.ch/registration2022">FS Switzerland</a></li>
 		  <li><a target="_blank" href="https://fs-alpeadria.com/fs-alpe-adria-event/registration">FS Alpe-Adria</a></li>
-		  <li><a target="_blank" href="https://fs-alpeadria.com/fs-alpe-adria-event/registration">FS Alpe-Adria Registration Procedure Guide</a></li>
+		  <li><a target="_blank" href="/doc/reg/FSAA_Registration_V2.pdf">FS Alpe-Adria Registration Procedure Guide</a></li>
         </ul>
       </div>
 
