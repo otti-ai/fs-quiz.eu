a8689ede239e75d44b2385b2dd1a92c594d49990
otti-ai <ottens@uni-bremen.de>
2022-12-13 13:00:41 +0100

    update dates and links

--- content/home.php
+++ content/home.php
@@ -12,7 +12,8 @@
 
     <hr class="col-3 col-md-2">
 	<h2>Dates</h2>
-	<img class="mx-auto d-block img-fluid" src="/img/dates.jpg">
+	<img class="mx-auto d-block img-fluid" src="/img/dates1.jpg">
+	<p>Testquiz from FS Switzerland on January 15, 13:00 CET</p>
 	<hr class="col-3 col-md-2">
 	
     <div class="row g-5">
@@ -20,13 +21,12 @@
         <h2>Registration</h2>
         <p>Information about the registration procedure</p>
         <ul class="icon-list">
-		  <li><a target="_blank" href="/doc/reg/FSA-Registration-Procedure-2022_1-1.pdf">FS Austria (v1.1)</a></li>
-		  <li><a target="_blank" href="/doc/reg/FSEast_Registration_guide_v2.pdf">FS East Registration Guide (v2)</a></li>
-		  <li><a target="_blank" href="https://fseast.eu/get-ready-for-fs-east-2022">FS East</a></li>
+		  <li><a target="_blank" href="/doc/reg/FSA-Registration-Procedure-2022_1-1.pdf">FS Austria (v1.1) (old)</a></li>
+		  <li><a target="_blank" href="/doc/reg/FSEast_Registration_guide_v2.pdf">FS East Registration Guide (v2) (old)</a></li>
+		  <li><a target="_blank" href="https://fseast.eu/get-ready-for-fs-east-2022">FS East (old)</a></li>
 		  <li><a target="_blank" href="https://www.formula-ata.it/how-to-register">SAE Italy</a></li>
-		  <li><a target="_blank" href="https://formulastudent.ch/registration2022">FS Switzerland</a></li>
-		  <li><a target="_blank" href="https://fs-alpeadria.com/fs-alpe-adria-event/registration">FS Alpe-Adria</a></li>
-		  <li><a target="_blank" href="/doc/reg/FSAA_Registration_V2.pdf">FS Alpe-Adria Registration Procedure Guide</a></li>
+		  <li><a target="_blank" href="https://formulastudent.ch/registration2022">FS Switzerland (old)</a></li>
+		  <li><a target="_blank" href="/doc/reg/FSAA_Registration_V2.pdf">FS Alpe-Adria Registration Procedure Guide (old)</a></li>
         </ul>
       </div>
 
@@ -34,15 +34,10 @@
         <h2>Documents</h2>
         <p>Important documents for the registration quizzes</p>
         <ul class="icon-list">
-          <li><a target="_blank" href="/doc/22/FS-Rules_2022_v1.0.pdf">FSG Rules 2022 (v1.0)</a></li>
-		  <li><a target="_blank" href="/doc/21/FS-Rules_2020_V1.0.pdf">FSG Rules 2021 (v1.0)</a></li>
-		  <li><a target="_blank" href="/doc/22/FS_2022_CV_Hybrid_Rules.pdf">FS Hybrid Rules 2022 (v0.9)</a></li>
-		  <li><a target="_blank" href="/doc/fseast22/FS_East_2022_Rules_v0.9.pdf">FS East Rules 2022 (v0.9)</a></li>
-		  <li><a target="_blank" href="/doc/fseast22/FS_East_Alumni_Rules_v1.0.pdf">FS East Alumni Cup Rules 2022 (v1.0)</a></li>
-		  <li><a target="_blank" href="/doc/fsg22/FSG22_Competition_Handbook_v1.1.pdf">FSG Handbook 2022 (v1.1)</a></li>
-		  <li><a target="_blank" href="/doc/fsn22/FSN-Competition-Handbook-2022-V1.0.pdf">FSN Handbook 2022 (v1.0)</a></li>
-		  <li><a target="_blank" href="/doc/fsa22/FSA-Competition-Handbook-2022_1-4-0-1.pdf">FSA Handbook 2022 (v1.4.0)</a></li>
-		  <li><a target="_blank" href="/doc/datalog/FSG-FSA-Datalogger-Datasheet-v0.1.pdf">FSG & FSA Datalogger 2022 (v0.1)</a></li>
+          <li><a target="_blank" href="/doc/23/FS-Rules_2023_v1.1.pdf">FSG Rules 2023 (v1.1)</a></li>
+		  <li><a target="_blank" href="/doc/23/FS-CV-Hybrid-Rules-Extension-2023-V1.1.pdf">FS Hybrid Rules 2023 (v1.1)</a></li>
+		  <li><a target="_blank" href="/doc/fseast23/FS_East_2023_Rules_Formatted_v1.0.pdf">FS East Rules 2023 (v1.0)</a></li>
+		  <li><a target="_blank" href="/doc/fsa23/FSA-Competition-Handbook-2023_1-1-0.pdf">FS Austria Competition Handbook 2023 (v1.1.0)</a></li>
         </ul>
       </div>
     </div>
