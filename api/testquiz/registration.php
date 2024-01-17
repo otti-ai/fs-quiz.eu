35964cdcf9658a23d813cf197edec1b06bdb12d4
otti-ai <ottens@uni-bremen.de>
2024-01-17 15:11:48 +0100

    fix index

--- api/testquiz/registration.php
+++ api/testquiz/registration.php
@@ -1,8 +1,15 @@
 <?php
 require($_SERVER['DOCUMENT_ROOT']. '/testquiz/datenbank.php');
 $token = bin2hex(random_bytes(8));
+$result = "error";
 $passwort_hash = password_hash($_GET["passwort"], PASSWORD_DEFAULT);
 $statement = $pdo->prepare("INSERT INTO `fs-testquiz-user` (`token`, `teamname`, `email`, `passwort`) VALUES (?, ?, ?, ?)");
-$statement->execute(array($token, $_GET["teamname"], $_GET["email"], $passwort_hash));
-echo "ok";
+try {
+    $statement->execute(array($token, $_GET["teamname"], $_GET["email"], $passwort_hash));
+    $result = "ok";
+}
+catch( PDOException $Exception ) {
+    $result = "error";
+}
+echo $result;
 ?>
