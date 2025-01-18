4699450be77e4a8c63430e51c19b312790334c68
otti-ai <yannik.ottens@botuniversity.de>
2025-01-18 17:55:49 +0100

    update home site

--- content/home.php
+++ content/home.php
@@ -14,21 +14,17 @@
 
     <div class="mb-2">
       <a href="/quizzes" class="btn btn-primary btn-lg px-4">Old quizzes</a>
-	  <a href="/advent" class="btn btn-danger btn-lg px-4" style="background: url(/img/advent/advent.jpg);">Advent Calendar</a>
     </div>
 
 	<hr class="col-3 col-md-2">
 
-	<!--<h2>Dates</h2>
-	  <ul class="icon-list">
-		  <li>FS-Quiz textquiz <a target="_blank" href="https://quiz.fs-quiz.eu/">information & result</a></li>
+	<h2>Dates</h2>
 			<?php require('./php/eventgraph.php'); ?>
-	  </ul>
-	<hr class="col-3 col-md-2">-->
-
+	<hr class="col-3 col-md-2">
+<div class="container m-0 p-0">
     <div class="row g-5">
       <div class="col-md-6">
-        <h2>Registration (old)</h2>
+        <h2>Registration</h2>
         <p>Information about the registration procedure</p>
         <ul class="icon-list">
           <?php
@@ -55,13 +51,15 @@
           <li><a target="_blank" href="https://www.formula-ata.it/how-to-register">SAE Italy</a></li>
 		  <li><a target="_blank" href="https://www.formulastudent.pt/registration-quiz">FS Portugal</a></li>
         </ul>
-		<!--<h2>Registration dates:</h2>
+		<h2>Registration dates:</h2>
 		 <p>Start of registrations on a first come, first served system</p>
 		<ul class="icon-list">
-          <li>19th January 9:00 CET <a target="_blank" href="https://www.formulastudent.ro/">FS Romania</a></li>
-		  <li>30th January 10:00 CET <a target="_blank" href="https://fstudent.ru/main/en">FS Russia</a></li>
-		  <li>5th February 10:00 CET <a target="_blank" href="https://www.formula-ata.it/">FS Italy</a></li>
-        </ul>-->
+      <li>31th January 9:00 CET <a target="_blank" href="https://www.formulastudent.ro/">FS Romania</a></li>
+      <li>1st February 13:00 CET <a target="_blank" href="#">FS Future</a></li>
+      <li>4th February 10:00 CET <a target="_blank" href="https://www.formula-ata.it/">FS Italy (early)</a></li>
+		  <li>7th February 10:00 CET <a target="_blank" href="https://www.formula-ata.it/">FS Italy (regular)</a></li>
+      <li>7th February 23:59 CET <a target="_blank" href="https://www.formula-student.nl/">FS Netherlands (deadline)</a></li>
+        </ul>
       </div>
 
       <div class="col-md-6">
@@ -100,6 +98,7 @@
         </ul>
       </div>
     </div>
+	 </div>
     <hr class="col-3 col-md-2 mb-5">
     <h2>API for quiz database</h2>
     <p class="fs-5">Below you will find the access and documentation of the available methods for the Quiz, Event, Solution and Image API.</p>
